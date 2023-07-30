c4c9413c1b400fb0f89a6d7383900c3a88844cb5
Annabelle Mittendorf Smith <annabellemittendorf@gmail.com>
2023-07-30 08:49:11 +0100

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -20,6 +20,7 @@ public class Greeter {
     String language = args[0];
     String country = args[1];
     String name = args[2];
+    //I am pretending to fix a bug 
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
   }
